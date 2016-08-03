3a008ed111741fa9efe03880764606c58127628c
kiuber <582279412@qq.com>
2016-08-03 20:29:31 +0800

    增加选择晚自习组织功能

--- app/src/main/java/com/goldenration/sdbi/xx/timechoose/activity/MainActivity.java
+++ app/src/main/java/com/goldenration/sdbi/xx/timechoose/activity/MainActivity.java
@@ -3,192 +3,295 @@ package com.goldenration.sdbi.xx.timechoose.activity;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.os.SystemClock;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.text.TextUtils;
-import android.view.KeyEvent;
+import android.text.format.DateFormat;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.goldenration.sdbi.xx.timechoose.R;
-import com.goldenration.sdbi.xx.timechoose.bean.User;
+import com.goldenration.sdbi.xx.timechoose.bean.Student;
+import com.goldenration.sdbi.xx.timechoose.util.GlideLoader;
+import com.yancy.imageselector.ImageConfig;
+import com.yancy.imageselector.ImageSelector;
+import com.yancy.imageselector.ImageSelectorActivity;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import cn.bmob.v3.Bmob;
 import cn.bmob.v3.BmobQuery;
 import cn.bmob.v3.listener.FindListener;
-import cn.bmob.v3.listener.SaveListener;
+import cn.bmob.v3.listener.UpdateListener;
 
 public class MainActivity extends AppCompatActivity {
-
-    private User mUserList;
-    private View view1;
-    private int currentViewId;
-    private View view2;
+    private Dialog dialog;
+    private String objectId;
+    private Student mStudent;
+    private TextView mTvStuID;
+    private TextView mTvStuName;
+    private TextView mTvTime;
+    private LinearLayout mLinearLayout;
+    private LinearLayout mLinearLayoutSelectPic;
+    private Spinner mSpinner;
+    private String[] mStrWhere =
+            {
+                    "去图书馆读书看报",
+                    "以老带新活动",
+                    "在班级固定教室学习和书写作业",
+                    "社团活动和系部专业工作室、互联网+学研基地活动",
+                    "参加体育锻炼和系部组织的体育比赛(系部统一组织)"
+            };
+    private EditText mEtRemarks;
+    private Button mBtnSelectPic;
+    private ImageView mIvSelectedPic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Bmob.initialize(this, "620ee288644d1af7c8ebd6f951a0e2a4");
-        view1 = View.inflate(this, R.layout.activity_main, null);
-        setContentView(view1);
-        if (getSharedPreferences("user_info", MODE_PRIVATE).getBoolean("isLogin", false)) {
-        } else {
-            login();
+        setContentView(R.layout.activity_main);
+        initView();
+        initData();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                do {
+                    try {
+                        Thread.sleep(1000);
+                        Message msg = new Message();
+                        msg.what = 1;  //消息(一个整型值)
+                        mHandler.sendMessage(msg);// 每隔1秒发送一个msg给mHandler
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                } while (true);
+            }
+        }).start();
+        if (!(getSharedPreferences("stu_info", MODE_PRIVATE).getBoolean("IsLogin", false))) {
+            Login();
+            mLinearLayout.setVisibility(View.GONE);
         }
-        findViewById(R.id.btn_commit).setOnClickListener(new View.OnClickListener() {
+    }
+
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case 1:
+                    SimpleDateFormat formatter = new SimpleDateFormat("yyyy年MM月dd日    HH:mm:ss     ");
+                    Date curDate = new Date(System.currentTimeMillis());//获取当前时间
+                    String sysTimeStr = formatter.format(curDate);
+                    mTvTime.setText(sysTimeStr);
+                    break;
+            }
+        }
+    };
+
+    private void initView() {
+        mLinearLayout = (LinearLayout) findViewById(R.id.ll_stu_name);
+        mLinearLayoutSelectPic = (LinearLayout) findViewById(R.id.ll_select_pic);
+        mTvTime = (TextView) findViewById(R.id.tv_time);
+        mTvStuID = (TextView) findViewById(R.id.tv_stu_id);
+        mTvStuName = (TextView) findViewById(R.id.tv_stu_name);
+        mSpinner = (Spinner) findViewById(R.id.spinner);
+        mSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (position == 0) {
+                    if (mEtRemarks.getVisibility() != View.GONE) {
+                        mEtRemarks.setVisibility(View.GONE);
+                    }
+                } else {
+                    if (mEtRemarks.getVisibility() != View.VISIBLE) {
+                        mEtRemarks.setVisibility(View.VISIBLE);
+                    }
+                }
+
+                if (position == 3) {
+                    if (mLinearLayoutSelectPic.getVisibility() != View.VISIBLE) {
+                        mLinearLayoutSelectPic.setVisibility(View.VISIBLE);
+                    }
+                } else {
+                    if (mLinearLayoutSelectPic.getVisibility() != View.GONE) {
+                        mLinearLayoutSelectPic.setVisibility(View.GONE);
+                    }
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        mEtRemarks = (EditText) findViewById(R.id.et_remarks);
+        mBtnSelectPic = (Button) findViewById(R.id.btn_select_pic);
+        mIvSelectedPic = (ImageView) findViewById(R.id.iv_selected_pic);
+        mBtnSelectPic.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                view2 = View.inflate(MainActivity.this, R.layout.view_change_pwd, null);
-                setContentView(view2);
+                ImageConfig imageConfig
+                        = new ImageConfig.Builder(new GlideLoader())
+                        .steepToolBarColor(getResources().getColor(R.color.blue))
+                        .titleBgColor(getResources().getColor(R.color.blue))
+                        .titleSubmitTextColor(getResources().getColor(R.color.white))
+                        .titleTextColor(getResources().getColor(R.color.white))
+                        // 开启单选   （默认为多选）
+                        .singleSelect()
+                        // 开启拍照功能 （默认关闭）
+                        .showCamera()
+                        // 拍照后存放的图片路径（默认 /temp/picture） （会自动创建）
+                        .filePath("/ImageSelector/Pictures")
+                        .build();
+
+                ImageSelector.open(MainActivity.this, imageConfig);   // 开启图片选择器
             }
         });
     }
 
-    private void login() {
-        final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-        builder.setTitle("登陆");
-        View view = View.inflate(MainActivity.this, R.layout.view_login, null);
-        builder.setView(view);
-        builder.setCancelable(false);
+    private void initData() {
+        String mStrStuID = getSharedPreferences("stu_info", MODE_PRIVATE).getString("Stu_ID", "");
+        if (mStrStuID != null) {
+            mTvStuID.setText(mStrStuID);
+        }
+        String mStrStuName = getSharedPreferences("stu_info", MODE_PRIVATE).getString("Stu_Name", "");
+        if (mStrStuName != null) {
+            mTvStuName.setText(mStrStuName);
+        }
+    }
 
-        final Dialog dialog = builder.create();
-        dialog.show();
-        final EditText mEtStuID = (EditText) view.findViewById(R.id.et_stu_id);
-        final EditText mEtPwd = (EditText) view.findViewById(R.id.et_pwd);
-        view.findViewById(R.id.btn_login).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                BmobQuery<User> userBmobQuery = new BmobQuery<User>();
-                userBmobQuery.addWhereEqualTo("User_StuID", mEtStuID.getText().toString());
-                userBmobQuery.findObjects(MainActivity.this, new FindListener<User>() {
+    private void Login() {
+        final View view = View.inflate(MainActivity.this, R.layout.view_login, null);
+        buildDialog(1, "登陆", null, view);
+    }
+
+    private void buildDialog(int flag, String title, String message, final View view) {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle(title);
+        if (message != null) {
+            builder.setMessage(message);
+        }
+        if (view != null) {
+            builder.setView(view);
+        }
+        switch (flag) {
+            case 1:
+                Button mBtnLogin = (Button) view.findViewById(R.id.btn_login);
+                mBtnLogin.setOnClickListener(new View.OnClickListener() {
                     @Override
-                    public void onSuccess(List<User> list) {
-                        if (list.size() == 0) {
-                            Toast.makeText(MainActivity.this, "没有此学号，请联系开发人员", Toast.LENGTH_SHORT).show();
-                        } else {
-                            if (list.size() == 1) {
-                                mUserList = list.get(0);
-                                if (TextUtils.equals(mUserList.getUser_Pwd().trim(), mEtPwd.getText().toString().trim())) {
-                                    if (TextUtils.equals(mUserList.getUser_DefaultPwd(), "true")) {
-                                        showUserConfirmDialog();
-                                    }
-                                    Toast.makeText(MainActivity.this, "登陆成功", Toast.LENGTH_SHORT).show();
-                                    dialog.dismiss();
+                    public void onClick(View v) {
+                        EditText mEtStuID = (EditText) view.findViewById(R.id.et_stu_id);
+                        final BmobQuery<Student> studentBmobQuery = new BmobQuery<Student>();
+                        studentBmobQuery.addWhereEqualTo("Stu_ID", mEtStuID.getText().toString());
+                        studentBmobQuery.findObjects(MainActivity.this, new FindListener<Student>() {
+
+                            @Override
+                            public void onSuccess(List<Student> list) {
+                                mStudent = list.get(0);
+                                if (list.size() == 0) {
+                                    Toast.makeText(MainActivity.this, "没有此学号", Toast.LENGTH_SHORT).show();
                                 } else {
-                                    Toast.makeText(MainActivity.this, "密码错误", Toast.LENGTH_SHORT).show();
+                                    objectId = mStudent.getObjectId();
+                                    EditText mEtStuPwd = (EditText) view.findViewById(R.id.et_stu_pwd);
+                                    if (TextUtils.equals(mStudent.getStu_Pwd(), mEtStuPwd.getText().toString())) {
+                                        Toast.makeText(MainActivity.this, "登陆成功", Toast.LENGTH_SHORT).show();
+                                        if (TextUtils.equals(mStudent.getStu_DefaultPwdStatus(), "TRUE")) {
+                                            buildDialog(2, "提示", "请设置新密码", null);
+                                        }
+                                    } else {
+                                        Toast.makeText(MainActivity.this, "密码错误", Toast.LENGTH_SHORT).show();
+                                    }
                                 }
                             }
-                        }
-                    }
-
-                    @Override
-                    public void onError(int i, String s) {
-                        Toast.makeText(MainActivity.this, s, Toast.LENGTH_SHORT).show();
-                    }
-                });
-            }
-        });
-    }
-
-    private void showUserConfirmDialog() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-        builder.setTitle("身份确认");
-        View view = View.inflate(MainActivity.this, R.layout.view_user_confirm, null);
-        builder.setView(view);
-        builder.setCancelable(false);
-
-        final Dialog dialog = builder.create();
-        dialog.show();
-
-        final EditText mEtName = (EditText) view.findViewById(R.id.et_name);
-        view.findViewById(R.id.btn_confirm).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (TextUtils.equals(mEtName.getText().toString().trim(), mUserList.getUser_Name())) {
-                    Toast.makeText(MainActivity.this, "身份确认成功", Toast.LENGTH_SHORT).show();
-                    dialog.dismiss();
-                    showChangePwdDialog();
-                } else {
-                    Toast.makeText(MainActivity.this, "身份确认失败", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-    }
-
-    private void showChangePwdDialog() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-        builder.setTitle("修改默认密码");
-        View view = View.inflate(MainActivity.this, R.layout.view_change_pwd, null);
-        builder.setView(view);
-        builder.setCancelable(false);
-
-        Dialog dialog = builder.create();
-        dialog.show();
-
-        final EditText mEtNewPwd = (EditText) view.findViewById(R.id.et_new_pwd);
-        view.findViewById(R.id.btn_change).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                AlertDialog.Builder builder1 = new AlertDialog.Builder(MainActivity.this);
-                builder1.setTitle("密码确认");
-                final String mStrNewPwd = mEtNewPwd.getText().toString().trim();
-                builder1.setMessage("密码将改为\n\n" + mStrNewPwd + "\n\n切记密码，找回密码可能将会浪费几晚上的时间");
-                builder1.setCancelable(false);
-                builder1.setPositiveButton("确认修改", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        User user = new User();
-                        user.setUser_Pwd(mStrNewPwd);
-                        user.save(MainActivity.this, new SaveListener() {
-                            @Override
-                            public void onSuccess() {
-                                saveUserInfo(mUserList.getUser_StuID(), mUserList.getUser_Name());
-                                Toast.makeText(MainActivity.this, "密码修改成功", Toast.LENGTH_SHORT).show();
-                                Intent k = MainActivity.this.getPackageManager().getLaunchIntentForPackage("com.goldenration.sdbi.xx.choose");
-                                k.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                                MainActivity.this.startActivity(k);
-                            }
 
                             @Override
-                            public void onFailure(int i, String s) {
+                            public void onError(int i, String s) {
                                 Toast.makeText(MainActivity.this, s, Toast.LENGTH_SHORT).show();
                             }
                         });
                     }
                 });
-                Dialog dialog1 = builder1.create();
-                dialog1.show();
-                builder1.setNegativeButton("重修修改", null);
-            }
-        });
+                break;
+            case 2:
+                final View view1 = View.inflate(MainActivity.this, R.layout.view_change_pwd, null);
+                builder.setPositiveButton("去设置", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        buildDialog(3, "设置新密码", null, view1);
+                    }
+                });
+                break;
+            case 3:
+                final EditText mEtNewPwd = (EditText) view.findViewById(R.id.et_new_pwd);
+                view.findViewById(R.id.btn_change).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Student student = new Student();
+                        student.setStu_Pwd(mEtNewPwd.getText().toString());
+                        student.update(MainActivity.this, objectId, new UpdateListener() {
+                            @Override
+                            public void onSuccess() {
+                                Toast.makeText(MainActivity.this, "密码修改成功，请牢记！", Toast.LENGTH_LONG).show();
+                                finish();
+                                startActivity(getIntent());
+                                saveUserInfo(mStudent.getStu_ID(), mStudent.getStu_Name());
+                            }
+
+                            @Override
+                            public void onFailure(int i, String s) {
+                                Toast.makeText(MainActivity.this, s, Toast.LENGTH_LONG).show();
+                            }
+                        });
+                    }
+                });
+                break;
+        }
+//        ViewGroup parent = (ViewGroup) view.getParent();
+//        if (parent != null) {
+//            parent.removeView(view);
+//        }
+        dialog = builder.create();
+        dialog.setCancelable(false);
+        dialog.show();
     }
 
-    private void saveUserInfo(String userName, String userID) {
-        getSharedPreferences("user_info", MODE_PRIVATE).edit().putString("User_StuID", userName).commit();
-        getSharedPreferences("user_info", MODE_PRIVATE).edit().putString("User_Name", userID).commit();
+    private void saveUserInfo(String stuID, String stuName) {
+        getSharedPreferences("stu_info", MODE_PRIVATE).edit().putString("Stu_ID", stuID).commit();
+        getSharedPreferences("stu_info", MODE_PRIVATE).edit().putString("Stu_Name", stuName).commit();
 
-        getSharedPreferences("user_info", MODE_PRIVATE).edit().putBoolean("isLogin", true).commit();
+        getSharedPreferences("stu_info", MODE_PRIVATE).edit().putBoolean("IsLogin", true).commit();
     }
 
     @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if (keyCode == KeyEvent.KEYCODE_BACK) {
-            if (currentViewId == view2.getId()) {
-                setCurrentView(view1);
-            } else {
-                System.exit(0);
-            }
-            return false;
-        }
-        return false;
-    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == ImageSelector.IMAGE_REQUEST_CODE && resultCode == RESULT_OK && data != null) {
 
-    private void setCurrentView(View view) {
-        currentViewId = view.getId();
-        setContentView(view);
+            // Get Image Path List
+            List<String> pathList = data.getStringArrayListExtra(ImageSelectorActivity.EXTRA_RESULT);
+
+            for (String path : pathList) {
+                Toast.makeText(this, path, Toast.LENGTH_SHORT).show();
+                Glide.with(MainActivity.this).load(path).into(mIvSelectedPic);
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/goldenration/sdbi/xx/timechoose/bean/Student.java
@@ -0,0 +1,55 @@
+package com.goldenration.sdbi.xx.timechoose.bean;
+
+import cn.bmob.v3.BmobObject;
+
+/**
+ * Created by  on 2016/8/3.
+ */
+
+public class Student extends BmobObject {
+    private String Stu_ID;
+    private String Stu_Name;
+    private String Stu_Sex;
+    private String Stu_Pwd;
+    private String Stu_DefaultPwdStatus;
+
+    public String getStu_ID() {
+        return Stu_ID;
+    }
+
+    public void setStu_ID(String stu_ID) {
+        Stu_ID = stu_ID;
+    }
+
+    public String getStu_Name() {
+        return Stu_Name;
+    }
+
+    public void setStu_Name(String stu_Name) {
+        Stu_Name = stu_Name;
+    }
+
+    public String getStu_Sex() {
+        return Stu_Sex;
+    }
+
+    public void setStu_Sex(String stu_Sex) {
+        Stu_Sex = stu_Sex;
+    }
+
+    public String getStu_Pwd() {
+        return Stu_Pwd;
+    }
+
+    public void setStu_Pwd(String stu_Pwd) {
+        Stu_Pwd = stu_Pwd;
+    }
+
+    public String getStu_DefaultPwdStatus() {
+        return Stu_DefaultPwdStatus;
+    }
+
+    public void setStu_DefaultPwdStatus(String stu_DefaultPwdStatus) {
+        Stu_DefaultPwdStatus = stu_DefaultPwdStatus;
+    }
+}
